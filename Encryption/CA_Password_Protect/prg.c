d85fcb3f8f2174bf244f2b04ce09761fa33287e0
Varul Srivastava <vs666@gmail.com>
2020-10-05 18:44:29 +0530

    Fixed bugs

--- Encryption/CA_Password_Protect/prg.c
+++ Encryption/CA_Password_Protect/prg.c
@@ -4,7 +4,7 @@
  * This method is space inefficient, but has more application in image compression than data compression
  * 
  * Created By : Varul Srivastava
- * Reviewed by : Ashwin Mittal
+ * Reviewed by : Varul Srivastava
  * 
  * This algorithm uses Wolfram's Rule of Cellular Automaton
 */
@@ -13,96 +13,103 @@
 #include <stdlib.h>
 #include <string.h>
 #include "util.h"
+#include "prg.h"
 
-const long long int MULT_NUM = (long long int)1000000007;
-const long long int ADD_NUM = (long long int)1029831027;
-const long long int MOD = (long long int)0xFF;
+const long long int MULT_NUM = (long long int)1092342049;
+const long long int ADD_NUM = (long long int)133942323;
+const long long int MOD = (long long int)256;
 int column_size = 100 * 1024 * 1024 / 8;
-const int RULE = 150;
+const int RULE = 30;
 int row_size = 1;
-char **hasharray;
+unsigned char **hasharray;
 int c_x = 0, c_y = 0;
 
-void initMain(char *pswd, int len)
+void initMain(unsigned char *pswd, int len)
 {
     row_size = max(len, strlen(pswd));
     column_size = column_size / row_size;
-    hasharray = (char *)malloc(column_size * sizeof(char *));
+    hasharray = (unsigned char **)malloc((column_size + 1) * sizeof(unsigned char *));
+    for (int x = 0; x < column_size + 1; x++)
+    {
+        hasharray[x] = (unsigned char *)malloc(row_size);
+    }
     hasharray = initPrg(pswd);
 }
 
-char conversion(char **base, int x, int y)
+unsigned char conversion(int x, int y)
 {
-    char bit_seq = 0;
+    unsigned char bit_seq = 0;
     for (int a = 0; a < 8; a++)
     {
         int num = 0;
         if (a == 0)
         {
-            num = (base[x][(y - 1 + row_size) % row_size] & (0x90));
+            num = (hasharray[x][(y - 1 + row_size) % row_size] & (1 << 7)) != 0 ? 1 : 0;
         }
         else
         {
-            num = (base[x][y] & (1 << (a - 1)));
-        }
-        num = num | (base[x][y] & (1 << a));
-        if (a == 7)
-        {
-            num = num | (base[x][(y + 1) % row_size] & (1));
-        }
-        else
-        {
-            num = num | (base[x][y + 1] & 1);
+            num = (hasharray[x][y] & (1 << (a - 1))) != 0 ? 1 : 0;
         }
 
-        bit_seq = bit_seq | (RULE & (1 << num));
+        num += (hasharray[x][y] & (1 << a)) != 0 ? 2 : 0;
+
+        if (a == 7)
+        {
+            num += (hasharray[x][(y + 1) % row_size] & (1)) != 0 ? 4 : 0;
+        }
+        else
+        {
+            num += (hasharray[x][y] & (1 << (a + 1))) != 0 ? 4 : 0;
+        }
+        // printf("NUM :: %d\n", num);
+        bit_seq += (RULE & (1 << num)) != 0 ? (1 << a) : 0;
     }
     return bit_seq;
 }
 
 /**
- *  prevents dictionary attacks by removing character bias
+ *  prevents dictionary attacks by removing unsigned character bias
  *  NOTE : Check for uniqueness of passwords or rate of hash collisions 
 */
-char **removeBias(char *ar)
+unsigned char **removeBias(unsigned char *ar)
 {
-    char arr[column_size][row_size];
+    int ar_size = strlen(ar);
+    unsigned char **arr = hasharray;
     for (int x = 0; x < row_size; x++)
     {
         if (x == 0)
         {
-            arr[0][x] = (ar[x] * MULT_NUM + ADD_NUM) % MOD;
+            arr[0][x] = (ar[x % ar_size] * MULT_NUM + ADD_NUM);
         }
         else
         {
-            arr[0][x] = ((ar[x] * MULT_NUM + ADD_NUM) ^ arr[0][x - 1]) % MOD;
+            arr[0][x] = ((ar[x % ar_size] * MULT_NUM + ADD_NUM) ^ arr[0][x - 1]);
         }
     }
     return arr;
 }
 
-char **initPrg(char *pswd)
+unsigned char **initPrg(unsigned char *pswd)
 {
-    row_size = max(row_size, strlen(pswd));
-    char **base = removeBias(pswd);
+    unsigned char **base = removeBias(pswd);
     for (int x = 1; x < column_size + 1; x++)
     {
         for (int y = 0; y < row_size; y++)
         {
-            base[x][y] = conversion(base, x - 1, y);
+            base[x][y] = conversion(x - 1, y);
         }
     }
     return base;
 }
 
-char getNum()
+unsigned char getNum()
 {
-    char val = 0;
+
+    unsigned char val = 0;
     for (int x = 0; x < 8; x++)
     {
-        val = val | (((hasharray[x + c_x][c_y / 8] & (1 << (c_y % 8))) != 0) << x);
+        val += (hasharray[c_x + x][c_y / 8] & (1 << (c_y % 8))) != 0 ? (1 << x) : 0;
     }
-    return val;
     c_x += 8;
     if (c_x >= column_size)
     {
@@ -110,11 +117,12 @@ char getNum()
         c_x = 0;
         c_y++;
     }
-    if (c_y >= row_size)
+    if (c_y / 8 >= row_size)
     {
         // reinitialization of the grid
         hasharray = initPrg(hasharray[column_size]);
         c_x = 0;
         c_y = 0;
     }
+    return val;
 }
--- Encryption/CA_Password_Protect/prg.h
+++ Encryption/CA_Password_Protect/prg.h
@@ -1,10 +1,10 @@
 #ifndef __PSEUDO_BASIC___
 #define __PSEUDO_BASIC___
 
-void initMain(char *, int);
-char conversion(char **base, int x, int y);
-char **removeBias(char *ar);
-char **initPrg(char *pswd);
-int getNum();
+void initMain(unsigned char *, int);
+unsigned char conversion(int x, int y);
+unsigned char **removeBias(unsigned char *ar);
+unsigned char **initPrg(unsigned char *pswd);
+unsigned char getNum();
 
 #endif /* __PSEUDO_BASIC___ */
