145530b9b50dbb041066b96c57ee3fdf2b01a5f3
Varul Srivastava <vs666@gmail.com>
2020-10-07 00:13:33 +0530

    Added analysis of the algorithm

---
+++ Encryption/CA_Password_Protect/Analysis/analysis.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+
+plt.rcParams.update({'font.size': 12})
+file_name = input("Enter file name : ")
+
+fileob = open(file_name,'rb')
+
+X = []
+a = []
+for i in range(256):
+    a.append(i)
+
+while True:
+    char = fileob.read(1)
+    if not char:
+        break
+    X.append(ord(char))
+
+# fig, ax = plt.subplots(figsize=(25,10))
+plt.hist(X,a,range=256)
+plt.show()
+
+
+fileob.close()
