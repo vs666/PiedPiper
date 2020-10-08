78d0adb2fe8512be210a4ca68e110bc42524d579
Varul Srivastava <vs666@gmail.com>
2020-10-08 13:16:30 +0530

    Added rle code

---
+++ Compression/AddOns/rle.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "rle.h"
+
+typedef unsigned char byte;
+
+char *convert(char *inp)
+{
+    int index = 0;
+    int n = strlen(inp);
+    char *len = (char *)malloc(n * 2);
+    int x = 0;
+    while (x < n)
+    {
+        byte count = 1;
+        while (x < n - 1 && inp[x] == inp[x + 1] && count < (byte)254)
+        {
+            x++;
+            count++;
+        }
+        len[index++] = count;
+        len[index++] = inp[x];
+        x++;
+    }
+    return len;
+}
---
+++ Compression/AddOns/rle.h
@@ -0,0 +1,6 @@
+#ifndef __RLE__
+#define __RLE__
+
+char *convert(char *input_string);
+
+#endif /* __RLE__ */
